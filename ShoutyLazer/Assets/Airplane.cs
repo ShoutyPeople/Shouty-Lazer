a75143d8e23b494e042150d6a819a2b2a3a5776c
Kevin Bradshaw <gaeel@nanoleptic.net>
2016-11-11 17:48:22 +0100

    Spawning and removing

--- ShoutyLazer/Assets/Airplane.cs
+++ ShoutyLazer/Assets/Airplane.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Airplane : MonoBehaviour {
 
+	public GameObject hurt;
 	public float accel;
 	public float friction;
 	public float minDistance;
@@ -10,8 +11,13 @@ public class Airplane : MonoBehaviour {
 
 	public float turnLerp;
 
+	public float life;
+	public float timeToRemove;
+
 	Vector3 vel;
 	Quaternion rotation;
+
+	bool killed;
 
 	// Use this for initialization
 	void Start () {
@@ -20,14 +26,36 @@ public class Airplane : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 forward = rotation*Vector3.forward;
-		vel+=(forward*accel-vel*friction)*Time.deltaTime;
-		transform.position+=vel*Time.deltaTime;
-		if (transform.position.magnitude>maxDistance){
-			rotation = Quaternion.Slerp(rotation, Quaternion.LookRotation(-transform.position.normalized),turnLerp*Time.deltaTime);
-		} else if (transform.position.magnitude<minDistance){
-			rotation = Quaternion.Slerp(rotation, Quaternion.LookRotation(transform.position.normalized),turnLerp*Time.deltaTime);
+		if (life>0){
+			Vector3 forward = rotation*Vector3.forward;
+			vel+=(forward*accel-vel*friction)*Time.deltaTime;
+			transform.position+=vel*Time.deltaTime;
+			if (transform.position.magnitude>maxDistance){
+				rotation = Quaternion.Slerp(rotation, Quaternion.LookRotation(-transform.position.normalized),turnLerp*Time.deltaTime);
+			} else if (transform.position.magnitude<minDistance){
+				rotation = Quaternion.Slerp(rotation, Quaternion.LookRotation(transform.position.normalized),turnLerp*Time.deltaTime);
+			}
+			transform.rotation = Quaternion.LookRotation(vel.normalized);
+		} else {
+			if (!killed){
+				killed = true;
+				RemoveAfterSeconds ras = gameObject.AddComponent<RemoveAfterSeconds>();
+				ras.seconds = timeToRemove;
+			}
+			hurt.SetActive(true);
+			vel+=Physics.gravity*Time.deltaTime;
+			transform.position+=vel*Time.deltaTime;
+			Vector3 spinny = new Vector3(
+				Mathf.Cos(Time.time),
+				Mathf.Sin(Time.time),
+				0
+			);
+			transform.rotation = Quaternion.LookRotation(vel.normalized);
 		}
-		transform.rotation = Quaternion.LookRotation(vel.normalized);
 	}
+
+	void Hit(RaycastHit rch){
+		life-=Time.fixedDeltaTime*ShoutInput.ShoutPower;
+	}
+
 }
---
+++ ShoutyLazer/Assets/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spawner : MonoBehaviour {
+
+	public int Amount;
+	public GameObject prefab;
+	public float Range;
+
+	public float rate;
+
+	List<GameObject> spawned;
+	float countDown = 1;
+
+	int checky = 0;
+
+	// Use this for initialization
+	void Start () {
+		spawned = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (countDown<0){
+			if (spawned.Count<Amount){
+				Spawn();
+			}
+		} else {
+			countDown-=rate*Time.deltaTime;
+		}
+		if (spawned.Count>0){
+			checky = (checky+1)%spawned.Count;
+			if (spawned[checky]==null){
+				spawned.RemoveAt(checky);
+			}
+		}
+	}
+
+
+	void Spawn(){
+		GameObject go = (GameObject) GameObject.Instantiate(prefab, transform.position+Random.insideUnitSphere*Range, Quaternion.identity);
+		spawned.Add(go);
+		countDown = 1;
+	}
+}
