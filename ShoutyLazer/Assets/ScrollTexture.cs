6fac0f44a0a09c73b627bcb6bf842f6195aefe27
Kevin Bradshaw <gaeel@nanoleptic.net>
2016-11-11 16:15:26 +0100

    Better laser effect

---
+++ ShoutyLazer/Assets/ScrollTexture.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollTexture : MonoBehaviour {
+	
+	public Vector2 scrollSpeed;
+	Renderer rend;
+	Material mat;
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+		mat = rend.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mat.mainTextureOffset+=scrollSpeed*Time.deltaTime;
+	}
+}
