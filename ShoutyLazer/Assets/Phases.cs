3f8b2bf2965cb1e277e7ff1e9e99197d153cba38
Kevin Bradshaw <gaeel@nanoleptic.net>
2016-11-11 19:44:46 +0100

    Built city

---
+++ ShoutyLazer/Assets/Phases.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class Phases : MonoBehaviour {
+
+
+	public int currentPhase = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
