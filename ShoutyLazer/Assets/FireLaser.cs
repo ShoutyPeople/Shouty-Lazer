07278e5efd188bb9ea0b8be1714b5abe80be550d
Kevin Bradshaw <gaeel@nanoleptic.net>
2016-11-12 02:10:28 +0100

    Stuff (Explosions mostly)

--- ShoutyLazer/Assets/FireLaser.cs
+++ ShoutyLazer/Assets/FireLaser.cs
@@ -24,6 +24,7 @@ public class FireLaser : MonoBehaviour {
 			RaycastHit rch;
 			if (Physics.Raycast(this.transform.position, this.transform.forward, out rch, 1000)){
 				Debug.Log(rch.collider.name);
+				rch.collider.SendMessage("Hit", rch, SendMessageOptions.DontRequireReceiver);
 				rch.collider.transform.root.SendMessage("Hit", rch, SendMessageOptions.DontRequireReceiver);
 				Phases.energy+=Time.fixedDeltaTime;
 			}
