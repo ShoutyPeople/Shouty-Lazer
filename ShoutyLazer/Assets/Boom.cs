bd63c96d0bb70ab593ff501d4281d91ccb9f631a
Kevin Bradshaw <gaeel@nanoleptic.net>
2016-11-12 16:49:56 +0100

    KILL KILL KILL EVERYONE

---
+++ ShoutyLazer/Assets/Boom.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boom : MonoBehaviour {
+
+	public float boomTime;
+	public GameObject boomBit;
+	public float boomSpeed;
+
+	public float initRadius;
+	public float initCount;
+
+	float time = 0;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i<initCount; i++){
+			GameObject.Instantiate(boomBit,transform.position+Random.onUnitSphere*initRadius, Random.rotation);
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		time += Time.deltaTime;
+		Vector2 circ = Random.insideUnitCircle.normalized*(initRadius+boomSpeed*(time*time));
+		GameObject.Instantiate(boomBit,
+			transform.position+Quaternion.AngleAxis(Random.Range(0,360),transform.up)*new Vector3(circ.x, 0, circ.y),
+			Random.rotation);
+		if (time>boomTime){
+			GameObject.Destroy(gameObject);
+		}
+	}
+}
---
+++ ShoutyLazer/Assets/Rotatey.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotatey : MonoBehaviour {
+
+	public Vector3 rotate;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localRotation = Quaternion.Euler(rotate*Time.deltaTime) * transform.localRotation; 
+	}
+}
