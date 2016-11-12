ecb3c0bdf5e831152d2f092308352be16e396ca9
Kevin Bradshaw <gaeel@nanoleptic.net>
2016-11-12 17:33:53 +0100

    Move bedroom to main scene

---
+++ ShoutyLazer/Assets/AsyncLoad.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class AsyncLoad : MonoBehaviour {
+
+	// Use this for initialization
+	static AsyncOperation async;
+	void Start () {
+		async = SceneManager.LoadSceneAsync(1);
+		async.allowSceneActivation = false;
+	}
+	
+	// Update is called once per frame
+	public static void NOW () {
+		async.allowSceneActivation = true;
+	}
+}
