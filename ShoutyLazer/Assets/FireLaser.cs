2c40124770f52393da661ddcbbfb7cd031d9969a
Kevin Bradshaw <gaeel@nanoleptic.net>
2016-11-12 14:08:00 +0100

    Level building

--- ShoutyLazer/Assets/FireLaser.cs
+++ ShoutyLazer/Assets/FireLaser.cs
@@ -8,22 +8,11 @@ public class FireLaser : MonoBehaviour {
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (beFiring){
-			RaycastHit rch;
-			if (Physics.Raycast(this.transform.position, this.transform.forward, out rch, 1000)){
-				rch.collider.transform.root.SendMessage("VisHit", rch, SendMessageOptions.DontRequireReceiver);
-			}
-		}
-	}
 
 	void FixedUpdate () {
 		if (beFiring){
 			RaycastHit rch;
 			if (Physics.Raycast(this.transform.position, this.transform.forward, out rch, 1000)){
-				Debug.Log(rch.collider.name);
 				rch.collider.SendMessage("Hit", rch, SendMessageOptions.DontRequireReceiver);
 				rch.collider.transform.root.SendMessage("Hit", rch, SendMessageOptions.DontRequireReceiver);
 				Phases.energy+=Time.fixedDeltaTime;
---
+++ ShoutyLazer/Assets/RemoveAtRange.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveAtRange : MonoBehaviour {
+
+	Transform cam;
+	public float Range = 30;
+
+	// Use this for initialization
+	void Start () {
+		cam = FindObjectOfType<Camera>().transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.SqrMagnitude(cam.position-transform.position)>Range){
+			Debug.Log("pop");
+			GameObject.Destroy(gameObject);
+		}
+	}
+}
